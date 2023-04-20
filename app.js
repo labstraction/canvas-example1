6b371da1ac73fb491d5c87aadc2a3b3591ae1b0d
labstraction <45163263+labstraction@users.noreply.github.com>
2023-04-20 08:47:20 +0200

    Update app.js

--- app.js
+++ app.js
@@ -8,9 +8,13 @@ const ctx = myCanvas.getContext('2d');
 for (let i = 0; i < 100; i++) {
     
     const originX = Math.random() * 600;
+    //0.1 * 600 = 60
     const originy = Math.random() * 600;
+    //0.3 * 600 = 180
     const width = Math.random() * 100;
+    //0.01 * 100 = 1
     const height = Math.random() * 100;
+    //0.9 * 100 = 90
 
     ctx.strokeStyle = 'white'
 
